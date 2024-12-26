20eebf711af45f25bde3f6cf1330f755c350b13a
Nahid <mdnahidhosentuhin@gmail.com>
2024-12-26 23:34:02 +0600

    Different consecutive characters

---
+++ Different_Consecutive_Characters.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, c = 0;
+        string s;
+        cin >> n >> s;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (s[i - 1] == s[i])
+            {
+                c++;
+            }
+        }
+        cout << c << endl;
+    }
+    return 0;
+}
